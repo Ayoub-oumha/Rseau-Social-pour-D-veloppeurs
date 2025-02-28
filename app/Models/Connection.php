69dbf0d10ba13f4ead3acbf01c169414c56d6d44
ayoub oumha <ayyouboumha@gmail.com>
2025-02-28 21:20:14 +0000

    Add Connection, Post, Project, and Language models with relationships

---
+++ app/Models/Connection.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Connection extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'sender_id',
+        'receiver_id',
+        'status'
+    ];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function connectedUser()
+    {
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
+
+    public function sender()
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    public function receiver()
+    {
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
+    public function friends()
+    {
+        return $this->hasMany(Connection::class, 'sender_id')
+        ->where('status', 'accepte')
+        ->orWhere('receiver_id', $this->id);
+    }
+}
