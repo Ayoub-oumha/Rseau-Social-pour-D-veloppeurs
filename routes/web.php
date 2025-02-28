758eac5ab351e770312e8d0498ace99df15ceb75
ayoub oumha <ayyouboumha@gmail.com>
2025-02-28 21:23:47 +0000

    Add routes for profile management and connection handling with authentication

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,38 @@
 <?php
 
+use App\Http\Controllers\ConnectionController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [HomeController::class, 'index'])->name('dashboard');
+
+// Route::get('/', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    // Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.profile');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::patch('/profile/cover', [ProfileController::class, 'updateCover'])->name('profile.cover');
+    Route::patch('/profile/profile_image', [ProfileController::class, 'updateImage'])->name('profile.image.update');
+    Route::post('/profile/addLanguage', [ProfileController::class, 'addLanguage'])->name('add.langue');
+    Route::post('/profile/addProject', [ProfileController::class, 'addProject'])->name('add.project');
+
 });
+Route::middleware(['auth'])->group(function () {
+    Route::get('/connections', [ConnectionController::class, 'index'])->name('connections.index');
+    Route::post('/connections/{user}', [ConnectionController::class, 'sendRequest'])->name('connections.send');
+    Route::post('/connections/{user}/accept', [ConnectionController::class, 'acceptRequest'])->name('connections.accept');
+    Route::post('/connections/{user}/ignore', [ConnectionController::class, 'ignoreRequest'])->name('connections.ignore');
+    Route::delete('/connections/{user}', [ConnectionController::class, 'removeConnection'])->name('connections.remove');
+});
+Route::middleware('auth')->group(function () {
+    // Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+   
+});
+
+require __DIR__.'/auth.php';
