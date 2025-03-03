e088382dbde65052f3d08fe5cb7de002426cbdef
ayoub oumha <ayyouboumha@gmail.com>
2025-03-03 09:52:10 +0000

    Refactor profile update logic and enhance validation rules; add fillable properties to Post model

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -24,13 +24,13 @@ class ProfileUpdateRequest extends FormRequest
                 'email',
                 'max:255',
                 Rule::unique(User::class)->ignore($this->user()->id),
-                'headline' => ['required', 'string'],
-                'work' => ['required', 'string'],
-                'location' => ['required', 'string'],
-                'bio' => ['required', 'string'],
-                'github_url' => ['required', 'string'],
-                'website' => ['required', 'string'],
             ],
+            'headline' => ['required', 'string'],
+            'work' => ['required', 'string'],
+            'location' => ['required', 'string'],
+            'bio' => ['required', 'string'],
+            'github_url' => ['required', 'string'],
+            'website' => ['required', 'string'],
         ];
     }
 }
